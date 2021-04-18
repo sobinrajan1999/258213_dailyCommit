0f0c73faaaebf7f5897b1ede2618a7336bf8b6c4
sobinrajan1999 <sobinrajan1999@gmail.com>
2021-04-18 12:25:20 +0530

    some commits

--- print.py
+++ print.py
@@ -1,3 +1,13 @@
-print("hello world");
-print(1+2+3);
-print()
+print("hello world")
+print(1+2+3)
+print(1+3-4)
+# Python also carries out multiplication and division,
+# using an asterisk * to indicate multiplication and
+# a forward slash / to indicate division.
+print(2+(3*4))
+# This will give you float value
+print(10/4)
+print(2*0.75)
+print(3.4*5)
+# if you do not want float value then
+print(10//4)
