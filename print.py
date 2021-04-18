c337efadb1c6274073b39a1ca1808e07a24a9c12
sobinrajan1999 <sobinrajan1999@gmail.com>
2021-04-18 12:38:40 +0530

    some commits

--- print.py
+++ print.py
@@ -11,3 +11,13 @@ print(2*0.75)
 print(3.4*5)
 # if you do not want float value then
 print(10//4)
+# Exponential or power
+# base 2 power 4
+print(2**4)
+# Chain exponential
+print(2**3**2)
+# Square root (result will be in float)
+print(9**(1/2))
+# Remainder
+print(20%6)
+print(1.25%0.5)
