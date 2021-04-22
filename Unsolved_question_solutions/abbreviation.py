e04e0f68ed4305b37e5fe0e3ecad66e3e2642f8e
sobinrajan1999 <sobinrajan1999@gmail.com>
2021-04-23 01:18:48 +0530

    abbreviation commit

---
+++ Unsolved_question_solutions/abbreviation.py
@@ -0,0 +1,11 @@
+abb = input("Enter the word: ")
+if abb == 'lol':
+    print("Laugh out loud")
+elif abb == 'rofl':
+    print("Rolling on the floor laughing")
+elif abb == 'lmk':
+    print("Let me know")
+elif abb == 'smh':
+    print("Shaking my head")
+else:
+    print("don't know about this word")
