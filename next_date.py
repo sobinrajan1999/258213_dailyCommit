cf84d8d7c2d01dbd566da1c625865d7f9e239a88
sobinrajan1999 <sobinrajan1999@gmail.com>
2021-04-23 01:03:18 +0530

    next_date commit

---
+++ next_date.py
@@ -0,0 +1,33 @@
+leap_year = False
+month = 0
+year = int(input("Enter year: "))
+if year % 400 == 0:
+    leap_year = True
+elif year % 100 == 0:
+    leap_year = False
+elif year % 4 == 0:
+    leap_year = True
+else:
+    leap_year = True
+
+month = int(input('Enter month: '))
+if month in [1, 3, 5, 7, 8, 10, 12]:
+    month_days = 31
+elif month == 2 and leap_year == True:
+    month_days = 29
+elif month == 2 and leap_year == False:
+    month_days = 28
+else:
+    month_days = 30
+
+day = int(input("Enter day: "))
+
+if day < month_days:
+    day += 1
+else:
+    day = 1
+if month == 12:
+    month = 1
+    year+=1
+
+print("Next day is: {}-{}-{}".format(day, month, year))
